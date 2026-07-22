aeb1dd14b4cb0561090ec410caba05aa55032d0e
Maria de los Angeles Moreno <madelosa2m1979@gmail.com>
2026-07-22 18:28:49 -0300

    mudanças aula 5

--- aula_5/aula_5.js
+++ aula_5/aula_5.js
@@ -1,4 +1,4 @@
-const API_URL = "https://6a55780ce49d9eb2cc55c367.mockapi.io/produtos";
+const API_URL = "https://6a5fe634b1933e9d25fcc879.mockapi.io/produtos";
 
 async function buscarProdutos() {
   try {
@@ -16,17 +16,52 @@ function popularTabela(produtos) {
   for (const produto of produtos) {
     html += `
       <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td>${produto.id}</td>
+      <td>${produto.nome}</td>
+      <td>${produto.preco}</td>
+      <td>${produto.quantidade}</td>
+      <td>${calcularTotal(produto.preco, produto.quantidade)}</td>
+      <td>
+        <button class="btn btn-danger" onclick="apagarProduto(${produto.id})">
+          Remover
+        </button>
+        <button class="btn btn-primary">
+          Atualizar
+        </button>
+      </td> 
       </tr>
     `;
   }
+
+  const tbody = document.querySelector(`#table_produtos tbody`);
+  tbody.innerHTML = html;
 }
 
-function calcularTotal(preco, quantidade) {}
+function calcularTotal(preco, quantidade) {
+  const resultado = Number(preco) * Number(quantidade);
+  return resultado.toFixed(2);
+}
+
+async function apagarProduto(id) {
+  if (!confirm("Realmente deseja apagar este produto?")) {
+    return;
+  }
+
+  const url = `${API_URL}/${id}`;
+  try {
+    await fetch(url, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    console.error(error);
+    alert("Não foi possivel apagar este produto");
+  } finally {
+    buscarProdutos();
+  }
+}
+
+async function criarProduto() {}
+
+async function atualizarProduto(id) {}
 
 buscarProdutos();
