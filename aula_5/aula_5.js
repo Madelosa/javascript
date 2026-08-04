ce5d0e131507431e60fc1fd7c25809d0f9d937a8
Maria de los Angeles Moreno <madelosa2m1979@gmail.com>
2026-08-03 21:41:43 -0300

    subir aula 03/08

--- aula_5/aula_5.js
+++ aula_5/aula_5.js
@@ -25,8 +25,8 @@ function popularTabela(produtos) {
         <button class="btn btn-danger" onclick="apagarProduto(${produto.id})">
           Remover
         </button>
-        <button class="btn btn-primary">
-          Atualizar
+        <button class="btn btn-primary" onclick="editarProduto(${produto.id})">
+          Editar
         </button>
       </td> 
       </tr>
@@ -60,12 +60,61 @@ async function apagarProduto(id) {
   }
 }
 
-async function criarProduto() {
-  const produto = {
+function modalNovoProduto() {
+  limparFormulario();
+  abrirModal();
+}
+
+async function editarProduto(id) {
+  const url = `${API_URL}/${id}`;
+
+  try {
+    const resposta = await fetch(url);
+    const produto = await resposta.json();
+    popularFormulario(produto);
+    abrirModal();
+  } catch (error) {
+    alert("Não foi possível editar este produto");
+  }
+}
+function salvarProduto() {
+  const id = Number(document.querySelector("#id").value) || 0;
+  const nome = document.querySelector("#nome").value;
+  const preco = document.querySelector("#preco").value;
+  const quantidade = document.querySelector("#quantidade").value;
+  //Não permitir campos vazios
+
+  if (nome == "" || preco == "" || quantidade == "") {
+    alert("Todos os campos são obrigatórios");
+    return;
+  }
+
+  //Não permitir valores não númericos para preco
+
+  if (!Number(preco) || !Number(quantidade)) {
+    alert("Campo preco e quantidade devem ser númericos.");
+    return;
+  }
+
+  if (id) {
+    atualizarProduto(id);
+    return;
+  }
+
+  criarProduto();
+}
+
+function criarObjetoProduto() {
+  return {
     nome: document.querySelector("#nome").value,
     preco: document.querySelector("#preco").value,
     quantidade: document.querySelector("#quantidade").value,
   };
+}
+
+async function criarProduto() {
+  const produto = criarObjetoProduto();
+
   try {
     await fetch(API_URL, {
       method: "POST",
@@ -81,11 +130,44 @@ async function criarProduto() {
     alert("Não foi possível adicionar o produto");
   }
 }
+async function atualizarProduto(id) {
+  const produto = criarObjetoProduto();
+  const url = `${API_URL}/${id}`;
+
+  try {
+    await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(produto),
+    });
+    limparFormulario();
+    fecharModal();
+    buscarProdutos();
+  } catch (error) {
+    alert("Não foi possível editar o produto");
+  }
+}
+
+function popularFormulario(produto) {
+  document.querySelector("#id").value = produto.id;
+  document.querySelector("#nome").value = produto.nome;
+  document.querySelector("#preco").value = produto.preco;
+  document.querySelector("#quantidade").value = produto.quantidade;
+}
 
 function limparFormulario() {
+  document.querySelector("#id").value = "";
   document.querySelector("#nome").value = "";
   document.querySelector("#preco").value = "";
   document.querySelector("#quantidade").value = "";
+}
+
+function abrirModal() {
+  const modalHtml = document.querySelector("#modalProduto");
+  const modal = bootstrap.Modal.getOrCreateInstance(modalHtml);
+  modal.show();
 }
 
 function fecharModal() {
@@ -93,7 +175,5 @@ function fecharModal() {
   const modal = bootstrap.Modal.getOrCreateInstance(modalHtml);
   modal.hide();
 }
-
-async function atualizarProduto(id) {}
 
 buscarProdutos();
